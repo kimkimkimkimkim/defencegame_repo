b88f38cbe8c2588bb3cdba8baecf5000bb306366
kimkimkimkimkim <kim1219sangwoo@yahoo.co.jp>
2018-08-14 23:04:32 +0900

    【機能追加】UIの配置
    ・城、地面、空、ヒーロー、ヒーロー生成ボタンの配置
    ・ヒーロー生成ボタンでヒーローを生成

---
+++ defencegame/Assets/Scripts/GameScene/CreMonButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreMonButton : MonoBehaviour {
+
+	//オブジェクト参照
+	public GameObject heroPrefab;
+
+	//メンバ変数
+	private GameObject imageGrass;
+
+	void Start(){
+		imageGrass = GameObject.Find ("ImageGrass");
+	}
+
+	public void OnClick(){
+		GameObject hero = (GameObject)Instantiate (heroPrefab);
+		hero.transform.SetParent (imageGrass.transform, false);
+		hero.GetComponent<RectTransform> ().offsetMin = new Vector2 (238, 0);
+		//hero.transform.
+	}
+}
---
+++ defencegame/Assets/Scripts/GameScene/HeroManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroManager : MonoBehaviour {
+
+	//グローバル変数
+	public float speed;
+
+	//メンバ変数
+	private Vector2 heroPos;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void Update(){
+		//ヒーローの移動
+		heroPos = this.GetComponent<RectTransform> ().offsetMin;
+		this.GetComponent<RectTransform> ().offsetMin = new Vector2 (heroPos.x + speed, heroPos.y);
+		this.GetComponent<RectTransform> ().sizeDelta = new Vector2 (70, 70);
+	}
+}
